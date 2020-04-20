208f48437e7ffc17ee72b9e4388253b952ab9311
slyao <slyao@tencent.com>
2020-04-20 23:13:03 +0800

    add inline lookup extraction fn

--- extraction_function.go
+++ extraction_function.go
@@ -7,9 +7,35 @@ type RegisteredLookupExtractionFn struct {
 	Lookup string `json:"lookup"`
 }
 
+type InlineLookupExtractionFn struct {
+	Type                    string               `json:"type"`
+	LookUp                  *LookUpExtractionMap `json:"lookup,omitempty"`
+	RetainMissingValue      bool                 `json:"retainMissingValue,omitempty"`
+	ReplaceMissingValueWith string               `json:"replaceMissingValueWith,omitempty"`
+	Injective               bool                 `json:"injective,omitempty"`
+}
+
+type LookUpExtractionMap struct {
+	Type string                 `json:"type"`
+	Map  map[string]interface{} `json:"map"`
+}
+
 type TimeExtractionFn struct {
 	Type     string `json:"type"`
 	Format   string `json:"format"`
 	TimeZone string `json:"timeZone"`
 	Locale   string `json:"locale"`
+}
+
+func DimExFnInlineLookUp(lookups map[string]interface{}, retainMissingValue bool, replaceMissingValueWith string, injective bool) ExtractionFn {
+	return &InlineLookupExtractionFn{
+		Type:                    "lookup",
+		RetainMissingValue:      retainMissingValue,
+		ReplaceMissingValueWith: replaceMissingValueWith,
+		Injective:               injective,
+		LookUp: &LookUpExtractionMap{
+			Type: "map",
+			Map:  lookups,
+		},
+	}
 }
