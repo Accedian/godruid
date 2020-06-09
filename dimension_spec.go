2a223f1af558a52f0f7c7a70731188bd27c37d6f
slyao <slyao@tencent.com>
2020-06-09 21:56:23 +0800

    fix test build

--- dimension_spec.go
+++ dimension_spec.go
@@ -3,10 +3,10 @@ package godruid
 type DimSpec interface{}
 
 type Dimension struct {
-	Type         string           `json:"type"`
-	Dimension    string           `json:"dimension"`
-	OutputName   string           `json:"outputName"`
-	ExtractionFn *DimExtractionFn `json:"extractionFn,omitempty"`
+	Type         string      `json:"type"`
+	Dimension    string      `json:"dimension"`
+	OutputName   string      `json:"output_name"`
+	ExtractionFn interface{} `json:"extractionFn,omitempty"`
 }
 
 type DimExtractionFn struct {
